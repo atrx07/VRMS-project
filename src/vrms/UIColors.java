c24babe349b61fdf91c211a66e928af425e0040a
Arppith Andrews <141434243+atrx07@users.noreply.github.com>
2026-08-26 21:42:04 +0530

    Add colors for vehicle card UI

--- src/vrms/UIColors.java
+++ src/vrms/UIColors.java
@@ -13,7 +13,15 @@ public class UIColors {
     public static final Color BG_LEFT = new Color(226, 239, 252);
     public static final Color BG_PAGE = new Color(248, 251, 254);
     public static final Color BG_SECONDARY_BTN = new Color(242, 245, 248);
+    public static final Color CARD_BG = Color.WHITE;
     public static final Color BORDER = new Color(218, 223, 230);
     public static final Color BORDER_DARK = new Color(205, 211, 218);
     public static final Color LINK = new Color(54, 122, 196);
+
+    public static final Color SUCCESS = new Color(35, 128, 78);
+    public static final Color SUCCESS_BG = new Color(226, 246, 235);
+    public static final Color WARNING = new Color(155, 102, 0);
+    public static final Color WARNING_BG = new Color(255, 244, 204);
+    public static final Color DANGER = new Color(181, 55, 55);
+    public static final Color DANGER_BG = new Color(252, 232, 232);
 }
